dd10d2e4692045d2b87b196df7feb943ed272996
Kadin Hatchell <kadinahatchell@gmail.com>
2016-09-27 08:36:07 -0600

    added question and response

--- src/popups/view/popupViewer.java
+++ src/popups/view/popupViewer.java
@@ -12,5 +12,18 @@ public class popupViewer
 	{
 		JOptionPane.showMessageDialog(null, message);
 	}
+	/**
+	 * Creates an input popup to collect a response from the user as a String.
+	 * @param question The question being asked to the user.
+	 * @return The user's response.
+	 */
+	public String collectResponse(String question)
+	{
+		String response = " ";
+		
+		response = JOptionPane.showInputDialog(null, question);
+		
+		return response;
+	}
 
 }
